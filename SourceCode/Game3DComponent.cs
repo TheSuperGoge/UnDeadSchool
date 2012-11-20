695063866b96e6a556e875b2efb0d539ed44d57a
TheSuperGoge <gogeultime@hotmail.com>
2012-11-20 13:35:39 -0500

    moved source code to folder

--- SourceCode/Game3DComponent.cs
+++ SourceCode/Game3DComponent.cs
@@ -11,14 +11,21 @@ using Microsoft.Xna.Framework.Media;
 
 namespace UnDeadSchool
 {
-    public class Game3DComponent : GameComponent
+    public class Game3DComponent : DrawableGameComponent
     {
         Vector3 position_;
+        Matrix world_;
 
         public Game3DComponent(Game game, Vector3 position)
             : base(game)
         {
             position_ = position;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            world_ = Matrix.CreateBillboard(position_, position_ - Vector3.Forward, Vector3.Up, null);
+            base.Update(gameTime);
         }
     }
 }
