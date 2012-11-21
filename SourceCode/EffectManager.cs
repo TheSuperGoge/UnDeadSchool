002359e9a86a6bcb72dcc661d1d7cb2415bf56be
TheSuperGoge <gogeultime@hotmail.com>
2012-11-21 16:20:51 -0500

    Added EffectManager class

---
+++ SourceCode/EffectManager.cs
@@ -0,0 +1,29 @@
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework;
+
+namespace UnDeadSchool
+{
+    public class EffectManager : GameComponent
+    {
+        public BasicEffect effect { get; protected set; }
+
+        public EffectManager(Game game)
+            : base(game)
+        {
+            effect = new BasicEffect(game.GraphicsDevice);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            Camera cam = (Camera)Game.Services.GetService(typeof(Camera));
+            effect.View = cam.View;
+            effect.Projection = cam.Projection;
+            Game.GraphicsDevice.SamplerStates[0] = SamplerState.PointClamp;
+            base.Update(gameTime);
+        }
+    }
+}
